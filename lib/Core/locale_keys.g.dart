126140a9ff5e6a170756ab6746f559e79c02e0c8
Abdelrahman <abdelrahmanhamdi08@gmail.com>
2026-02-20 19:52:49 +0200

    feat: add translations to the app and add locale keys and en.json and ar.json

---
+++ lib/Core/locale_keys.g.dart
@@ -0,0 +1,22 @@
+abstract class LocaleKeys{
+  static const String kEmail = 'kEmail';
+  static const String kPassword = 'kPassword';
+  static const String kLogin = 'kLogin';
+  static const String kDoNotHaveAccount = 'kDoNotHaveAccount';
+  static const String kSignup = 'kSignup';
+  static const String kName = 'kName';
+  static const String kPhoneNumber = 'kPhoneNumber';
+  static const String kConfirmPassword = 'kConfirmPassword';
+  static const String kAlreadyHaveAccount = 'kAlreadyHaveAccount';
+  static const String kNotifications = 'kNotifications';
+  static const String kNoNotificationsYet = 'kNoNotificationsYet';
+  static const String kMyCourses = 'kMyCourses';
+  static const String kCourseDetails = 'kCourseDetails';
+  static const String kDuration = 'kDuration';
+  static const String kPrice = 'kPrice';
+  static const String kDescription = 'kDescription' ;
+  static const String kEnroll = 'kEnroll';
+  static const String kMyCourseDetails = 'kMyCourseDetails';
+  static const String kAttemptExam = 'kAttemptExam';
+
+}
--- lib/couraty.dart
+++ lib/couraty.dart
@@ -1,20 +1,26 @@
 import 'package:coursaty/core/routing/app_router.dart';
 import 'package:coursaty/core/Themes/themes.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
 
 class CoursatyApp extends StatelessWidget {
   const CoursatyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return ScreenUtilInit(
-      designSize: const	Size(360,690),
+      designSize: const Size(360, 690),
       useInheritedMediaQuery: true,
       ensureScreenSize: true,
       builder: (context, child) {
         return MaterialApp.router(
+          locale: context.locale,
+          supportedLocales: context.supportedLocales,
+          localizationsDelegates: context.localizationDelegates,
+
           debugShowCheckedModeBanner: false,
           title: 'Coursaty',
           themeMode: ThemeMode.light,
