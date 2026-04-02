f8ee6df28da665acb30e9f80a22b0592dbf7374b
Abdelrahman <abdelrahmanhamdi08@gmail.com>
2026-04-02 03:10:07 +0200

    remove unneeded bloc consumer

--- lib/Core/Shared_Widgets/functions/show_cheating_dialog.dart
+++ lib/Core/Shared_Widgets/functions/show_cheating_dialog.dart
@@ -5,7 +5,6 @@ import 'package:coursaty/Core/Themes/style_data.dart';
 import 'package:coursaty/Core/Themes/unit.dart';
 import 'package:coursaty/Core/locale_keys.g.dart';
 import 'package:coursaty/features/user/prsentation/manager/user_cubit.dart';
-import 'package:coursaty/features/user/prsentation/manager/user_state.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
